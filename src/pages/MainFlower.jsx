090f75cee81b866a09487916b34cb33f2dcc3481
Thomas-Culham <68413791+Thomas-Culham@users.noreply.github.com>
2023-05-29 15:19:07 +1200

    flower images

--- src/pages/MainFlower.jsx
+++ src/pages/MainFlower.jsx
@@ -1,13 +1,23 @@
+import seeds from '../assets/images/see.png';
+import flower2 from '../assets/images/flower2.png';
+import flower3 from '../assets/images/flower3.png';
 
-import flower from '../assets/images/flowerTest.png'
+const allImages = [flower1, flower2, flower3];
 
 function MainFlower() {
-    return (
-        <div>
-            <h3>Your flower</h3>
-            <img src={flower} alt="flower" className="flower-image" />
-        </div>
-    );
+  // logic to determine the index of the flower to display
+  const indexToDisplay = 1;
+
+  return (
+    <div>
+      <h3>Your flower</h3>
+      <img
+        src={allImages[indexToDisplay]}
+        alt={`flower ${indexToDisplay + 1}`}
+        className="flower-image"
+      />
+    </div>
+  );
 }
 
 export default MainFlower;
