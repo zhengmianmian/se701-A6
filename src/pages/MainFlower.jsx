a7a9e60955ef9e3bb8a31f17d7f03e529e51f550
Thomas-Culham <68413791+Thomas-Culham@users.noreply.github.com>
2023-05-29 15:23:05 +1200

    flowers imported

--- src/pages/MainFlower.jsx
+++ src/pages/MainFlower.jsx
@@ -1,8 +1,13 @@
-import seeds from '../assets/images/see.png';
-import flower2 from '../assets/images/flower2.png';
-import flower3 from '../assets/images/flower3.png';
+import seeds from '../assets/images/flowers/seeds.png';
+import plantedSeeds from '../assets/images/flowers/plantedSeeds.png';
+import quizz_1 from '../assets/images/flowers/1quizz.png';
+import quizz_2 from '../assets/images/flowers/2quizz.png';
+import quizz_3 from '../assets/images/flowers/3quizz.png';
+import quizz_4 from '../assets/images/flowers/4quizz.png';
+import evolution_1 from '../assets/images/flowers/evolution1.png';
+import evolution_2 from '../assets/images/flowers/evolution2.png';
 
-const allImages = [flower1, flower2, flower3];
+const allImages = [seeds, plantedSeeds, quizz_1, quizz_2, quizz_3, quizz_4, evolution_1, evolution_2];
 
 function MainFlower() {
   // logic to determine the index of the flower to display
