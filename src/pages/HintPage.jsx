c9bedaceae01f067681a4d5e94a8bf068ed4ed8a
Koe2200 <79816353+Koe2200@users.noreply.github.com>
2023-05-30 18:04:23 +1200

    ChatGPT and Hint Page
    Yes

--- src/pages/HintPage.jsx
+++ src/pages/HintPage.jsx
@@ -1,20 +1,42 @@
+<<<<<<< Updated upstream
 function HintPage(props) {
 
     return (
         <div className="container">
             <h1 className="title" style={{textAlign:"left", marginLeft: '5%', fontSize: '49px'}}>Flower Math.</h1>
+=======
+import {useLocation} from "react-router-dom";
+import React from "react";
+
+function HintPage(props) {
+    const location = useLocation()
+    const dict = {
+        "geometry1": ['Square and rectangle has 4 sides', 'A triangle has 3', 'A circle only has 1 side', 'How many sides are on the shape you see?', 'Triangles'],
+        "geometry2": 'Question2'
+    }
+    //To get the data sent between web page
+    //{dict[location.state.id]}
+
+    //To send the data between web page (Place in <Link /Link>)
+    //state={{ id : "geometry2" }}
+
+    return (
+        <div className="container">
+            <h1 className="title" style={{textAlign:"left", marginLeft: '5%', fontSize: '49px'}}>Math Gardener</h1>
+>>>>>>> Stashed changes
             <div className="content" >
                 <p className="subtitle" style={{ color: '#38b6ff', textAlign: 'left', marginLeft: '7%', fontSize: '25px'}}>Hint:</p>
-                <ul style={{ textAlign: 'left', marginLeft: '5%', marginTop: '1%'}}> From the figure, you should be able to see the following information:<br />
+                <ul style={{ textAlign: 'left', marginLeft: '5%', marginTop: '1%'}}> Here's a hint to your question:<br />
                     <br />
-                    <li style={{marginLeft: "2%"}}>The radius length of the inner sector equals the side length of the triangle and its central angle is 30°.<br /></li>
-                    <li style={{marginLeft: "2%"}}>The radius length of the larger sector equals the base length of the triangle, and its central angle equals the base angle.<br /></li>
+                    <li style={{marginLeft: "2%"}}>{dict[location.state.id][0]}<br /></li>
+                    <li style={{marginLeft: "2%"}}>{dict[location.state.id][1]}<br /></li>
+                    <li style={{marginLeft: "2%"}}>{dict[location.state.id][2]}<br /></li>
                     <br />
-                    So, the area of the shaded part is the area of the larger sector minus the area of the smaller sector. </ul>
+                    {dict[location.state.id][3]} </ul>
                 <p style={{ color: '#1ea6bc', fontSize: '23px', textAlign: 'left', marginLeft: '17%', marginTop: '10%'}}>Ask ChatGPT</p>
                 <div style={{ display: 'flex', alignItems: 'center'}}>
                     <div style={{ marginRight: '10%' }}>
-                        <img src="AcuteAngle.jpg" alt="Acute Angle" />
+                        <img style={{width:170, height:170, alignSelf: 'center', marginLeft: '15%'}} src="./assets/images/" alt={dict[location.state.id][4]} />
                     </div>
                     <ul style={{textAlign: 'left', marginTop: '1%'}}>You can ask ChatGPT the following questions and try to answer the question by yourself:<br />
                         <br />
