cbfbc598fb01af40512503196b733cbe32a7aa91
Thomas-Culham <68413791+Thomas-Culham@users.noreply.github.com>
2023-06-01 22:44:02 +1200

    Update LearnPage.jsx

--- src/pages/LearnPage.jsx
+++ src/pages/LearnPage.jsx
@@ -4,7 +4,7 @@ import { useNavigate} from 'react-router-dom';
 import React from 'react';
 import { Card, CardMedia } from '@mui/material';
 import roadmap from '../assets/images/topic_roadmap.png'
-import { Container, Box, } from '@mui/material';
+import { Container, Box, Grid} from '@mui/material';
 
 function LearnPage(props) {
     const navigate = useNavigate();
@@ -39,29 +39,35 @@ function LearnPage(props) {
         <Button key={index} onClick={()=>navigate(item.path)} className="topicBtn" variant="contained" color="primary">{item.name}</Button>
     );
     return (
-        <Container>
-            <Box mt={2}>
-                <h1>Select a topic to learn</h1>
-            </Box>
-            <Box my={2} className="btnContainer">
-                {topicItems}
-            </Box>
-            <Box my={2}>
-                <Card>
-                    <CardMedia
+        <Container style={{ fontSize: '24px', marginLeft: '-40px' }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                <Box mt={2}>
+                    <h1>Select a topic to learn</h1>
+                </Box>
+                <Box my={2} className="btnContainer">
+                    {topicItems}
+                </Box>
+                <Box my={2}>
+                    <h4>You can start with Geometry, Arithmetic, Prime Numbers, and Basic Algebra. After understanding Arithmetic, you can move on to Fractions. Once you have a good grasp of Basic Algebra, you can then proceed to Equations.</h4>
+                </Box>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                <Box my={2}>
+                    <Card style={{width:'800px' }}>
+                    <CardMedia 
                         component="img"
                         alt="Contemplative Reptile"
-                        style={{width: '100%', height: 'auto'}}
+                        style={{ width: '100%', height: 'auto' }}
                         image={roadmap}
                         title="Contemplative Reptile"
                     />
-                </Card>
-            </Box>
-            <Box my={2}>
-                <h4>You can start with Geometry, Arithmetic, Prime Numbers, and Basic Algebra. 
-                    After understanding Arithmetic, you can move on to Fractions. Once you have a good grasp of Basic Algebra, you can then proceed to Equations.</h4>
-            </Box>
-        </Container>
+                    </Card>
+                </Box>
+                </Grid>
+            </Grid>
+            </Container>
+
     );
 }
 
