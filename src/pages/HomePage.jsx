d939725628d3b8fa77d39e08e1d77ea9abab1a33
ZTianWang <wnwn1994@163.com>
2023-06-01 21:01:04 +1200

    change home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import f3 from '../assets/images/home_flowers/f3.PNG'
 import f4 from '../assets/images/home_flowers/f4.PNG'
 import '../assets/css/HomePage.css'
 import { Container, Box, Grid, Typography } from '@mui/material';
-import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis,} from 'recharts';
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, } from 'recharts';
 import CardMedia from '@mui/material/CardMedia';
 import Card from '@mui/material/Card';
 
@@ -17,47 +17,47 @@ const flowers = [
 
 const data = [
     {
-      subject: 'Geometry',
-      A: 50,
-      fullMark: 150,
+        subject: 'Geometry',
+        A: 50,
+        fullMark: 150,
     },
     {
-      subject: 'Arithmetic',
-      A: 90,
-      fullMark: 150,
+        subject: 'Arithmetic',
+        A: 90,
+        fullMark: 150,
     },
     {
-      subject: 'Fractions',
-      A: 90,
-      fullMark: 150,
+        subject: 'Fractions',
+        A: 90,
+        fullMark: 150,
     },
     {
-      subject: 'Prime Numbers',
-      A: 90,
-      fullMark: 150,
+        subject: 'Prime Numbers',
+        A: 90,
+        fullMark: 150,
     },
     {
-      subject: 'Basic Algebra',
-      A: 90,
-      fullMark: 150,
+        subject: 'Basic Algebra',
+        A: 90,
+        fullMark: 150,
     },
     {
-      subject: 'Equations',
-      A: 150,
-      fullMark: 150,
+        subject: 'Equations',
+        A: 150,
+        fullMark: 150,
     },
-  ];
+];
 
 function HomePage() {
-    const flowerItems = flowers.map((item,index)=>
+    const flowerItems = flowers.map((item, index) =>
         <Grid item xs={3} key={index}>
             <Box component="div" m={1} boxShadow={1} p={1} borderRadius={1}>
-                <Typography variant="h6" align="center">level {index+1}</Typography>
+                <Typography variant="h6" align="center">level {index + 1}</Typography>
                 <Card sx={{ width: '100%', height: 0, paddingBottom: '200%', position: 'relative', overflow: 'hidden' }}>
                     <CardMedia
                         component="img"
                         image={item}
-                        alt={`flower ${index+1}`}
+                        alt={`flower ${index + 1}`}
                         sx={{ position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, height: '100%', width: '100%', objectFit: 'cover' }}
                     />
                 </Card>
@@ -67,31 +67,34 @@ function HomePage() {
 
     return (
         <Container maxWidth="lg">
-            <Typography variant="h3" align="center" gutterBottom>Flowers and Skills</Typography>
-            <Grid container spacing={2}>
-                <Grid item xs={6}>
-                    <Box display="flex" justifyContent="center" alignItems="center" width="100%" my={1}>
-                        <RadarChart
-                            cx={450}
-                            cy={225}
-                            outerRadius={200}
-                            width={900}
-                            height={900}
-                            data={data}
-                        >
-                            <PolarGrid />
-                            <PolarAngleAxis dataKey="subject" />
-                            <PolarRadiusAxis />
-                            <Radar
-                                name="Mike"
-                                dataKey="A"
-                                stroke="#8884d8"
-                                fill="#8884d8"
-                                fillOpacity={0.6}
-                            />
-                        </RadarChart>
-                    </Box>
-                </Grid>
+            <Grid container spacing={2} style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+            }}>
+                <div>
+                    <Grid item xs={6}>
+                        <Box>
+                            <RadarChart
+                                width={700}
+                                height={500}
+                                data={data}
+                            >
+                                <PolarGrid />
+                                <PolarAngleAxis dataKey="subject" />
+                                <PolarRadiusAxis />
+                                <Radar
+                                    name="Mike"
+                                    dataKey="A"
+                                    stroke="#8884d8"
+                                    fill="#8884d8"
+                                    fillOpacity={0.6}
+                                />
+                            </RadarChart>
+                        </Box>
+                    </Grid>
+                </div>
                 <Grid item xs={6}>
                     <Grid container spacing={2}>
                         {flowerItems}
