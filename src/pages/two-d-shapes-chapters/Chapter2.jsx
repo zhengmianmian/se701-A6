f28fc5a2c5dffce8483c7eb306f2f78f5beb20b1
Thomas-Culham <68413791+Thomas-Culham@users.noreply.github.com>
2023-06-01 22:08:02 +1200

    chapters updated

--- src/pages/two-d-shapes-chapters/Chapter2.jsx
+++ src/pages/two-d-shapes-chapters/Chapter2.jsx
@@ -7,36 +7,46 @@ import perimeterLearn from '../../assets/images/Geometry/PerimeterLearn.jpg'
 function Chapter2(props) {
     const navigate = useNavigate();
     return (
-        <div >
+        <div>
             <h1>Chapter 2 - Sides and Angles</h1>
             <div>
                 <p>A perimeter is the total length around the outside of the shape</p>
             </div>
-            <Card style={{ padding: '10px', display: 'flex', justifyContent: 'center' }}>
-                <ReactPlayer
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Card style={{ width:'900px'}}>
+                <ReactPlayer 
                     url="https://youtu.be/MTSlKifo4js"
                     controls
-                    width="480px"
-                    height="360px"
+                    width="100%"
+                    height="100%"
                 />
-            </Card>
-            <Card style={{ padding: '10px', display: 'flex', justifyContent: 'center' }}>
-            <img src={perimeterLearn} alt="shapes" 
-                style={{width:"50%", height:"auto"}}
-                />
-            </Card>
-            <div>
-                <h2>Key Ideas</h2>
-                <p>Perimeter is the total length of the outside of a shape</p>
-                <p>Perimeter of a square: Side + Side + Side + Side</p>
-                <p></p>
-                
+                </Card>
+                <Card style={{marginLeft: '10px'}}>
+                <img
+                    src={perimeterLearn}
+                    alt="shapes"
+                    style={{ width: '50%', height: 'auto' }}/>
+                    <h2>Key Ideas</h2>
+                    <p>Perimeter is the total length of the outside of a shape. </p>
+                    <p>Perimeter of a square: Side + Side + Side + Side</p>
+                </Card>
             </div>
-            <div>
-                <Button style={{margin:"10px"}}  variant="contained" color="primary">Chapter 2 Quiz</Button>
-                <Button style={{margin:"10px"}} onClick={()=>{navigate('/discuss');}} variant="contained" color="primary">Discussion</Button>
-            </div>
-        </div>
+      <div>
+        <Button style={{ margin: '10px' }} variant="contained" color="primary">
+          Chapter 2 Quiz
+        </Button>
+        <Button
+          style={{ margin: '10px' }}
+          onClick={() => {
+            navigate('/discuss');
+          }}
+          variant="contained"
+          color="primary"
+        >
+          Discussion
+        </Button>
+      </div>
+    </div>
     );
 }
 
